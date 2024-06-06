212734276088c0560168bc5290875be7bfd4cb5b
poluektovdan <127665642+poluektovdan@users.noreply.github.com>
2024-06-06 14:57:32 +0300

    Добавление пагинации на главную страницу

--- core/function.php
+++ core/function.php
@@ -25,6 +25,32 @@ function select($conn) {
 	return $a;
 }
 
+function selectMain($conn) {
+	$offset = 0;
+    if (isset($_GET['page']) AND trim($_GET['page'])!=''){
+        $offset = trim($_GET['page']);
+    }
+	$sql = "SELECT * FROM info ORDER BY id DESC LIMIT 3 OFFSET ".$offset*3;
+	$result = mysqli_query($conn, $sql);
+
+	$a = array();
+
+	if (mysqli_num_rows($result) > 0) {
+	while($row = mysqli_fetch_assoc($result)) {
+		$a[] = $row;
+	}
+	}
+
+	return $a;
+}
+
+function paginationCount($conn){
+    $sql = "SELECT * FROM info";
+    $result = mysqli_query($conn, $sql);
+    $result = mysqli_num_rows($result);
+    return ceil($result/3);
+}
+
 function close($conn) {
 	mysqli_close($conn);
 }
--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?php
 require_once('core/config.php');
 require_once('core/function.php');
+
+$conn = connect();
+$data = selectMain($conn);
+$countPage = paginationCount($conn);
+close($conn);
 ?>
 
+<?php
+$out = '';
+for ($i=0; $i < count($data); $i++){
+    $out .="<img src='/images/{$data[$i]['image']}' width='100'>";
+    $out .="<h2>{$data[$i]['title']}</h2>";
+    $out .="<p>{$data[$i]['descr_min']}</p>";
+    $out .="<p><a href='/arcticle.php?id='{$data[$i]['id']}'>Read more...</a></p>";
+    $out.='<hr>';
+}
+echo $out;
+for ($i=0; $i < $countPage; $i++){
+    $j = $i+1;
+    echo "<a href='/index.php?page={$i}' style='padding: 5px;'>{$j}</a>";
+}
+?>
